14642c9b02172cc270c4726975fb37ac752c7b18
Marlon Santos <santosfmarlon@gmail.com>
2023-05-27 12:31:36 -0400

    postman review

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -9,7 +9,26 @@ servers_routes = Blueprint('servers', __name__)
 # ---- getting all servers
 @servers_routes.route('/')
 def get_all_servers():
-    if request.method == 'GET':
-        servers= db.session.query(Server).all()
+    servers= db.session.query(Server).all()
 
-        return {'servers':{server.id: server.to_resource_dict() for server in servers}}
+    return {'servers':{server.id: server.to_socket_dict() for server in servers}}
+
+# ---- creating a new server
+@servers_routes.route('/', methods=['POST'])
+def create_a_server():
+    server = Server(owner_id=current_user.id, name=request.form['name'])
+    db.session.add(server)
+    db.session.commit()
+
+    owner = ServerMember(server_id=server.id, user_id=server.owner_id)
+    db.session.add(owner)
+
+    generalChat = Channel(name="General Chat", server_id=server.id)
+    db.session.add(channel)
+    db.session.commit()
+
+    welcome_message = ChannelMessage(channel_id=generalChat.id, sender_id=current_user.id, content=f'Hey! Welcome to the {server.name} server')
+    db.session.add(welcome_message)
+    db.session.commit()
+
+    return server.to_dict()
